b33183aa76155a6d81dbaf5f62923d0174a8d669
Charlene Gipulan <charlenegipulan@gmail.com>
2018-09-29 14:30:33 -0700

    render basic landing page

---
+++ src/pages/LandingPage/LandingPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './LandingPage.css';
+
+class LandingPage extends React.Component {
+
+
+    render() {
+        return (
+            <div className="LandingPage">
+                Hello World
+            </div>
+        )
+    }
+}
+
+export default LandingPage;
