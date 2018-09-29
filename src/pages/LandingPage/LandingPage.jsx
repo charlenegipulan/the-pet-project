84c0ada7117de4798a335c9a4581b5c9699a64c7
Charlene Gipulan <charlenegipulan@gmail.com>
2018-09-29 14:59:34 -0700

    search bar

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -2,14 +2,18 @@ import React from 'react';
 import './LandingPage.css';
 import LandingBanner from '../../components/LandingBanner/LandingBanner';
 import NavBar from '../../components/NavBar/NavBar';
+
 class LandingPage extends React.Component {
 
 
     render() {
         return (
-            <div className="LandingPage">
+            <div className='LandingPage'>
                 <LandingBanner />
                 <NavBar />
+                <div className='SearchBar'>
+                    <input placeholder='I am looking for ...'/>
+                </div>
             </div>
         )
     }
