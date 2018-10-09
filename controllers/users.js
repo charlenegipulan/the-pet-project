c730a83d65b03bfafecd432073055343b3a115b2
Rachel Barker <rachelbarker001@gmail.com>
2018-10-09 15:02:25 -0700

    jwt step 3

--- controllers/users.js
+++ controllers/users.js
@@ -1,16 +1,26 @@
 var User = require('../models/user');
+var jwt = require('jsonwebtoken');
+var SECRET = process.env.SECRET;
+
 
 module.exports = {
-  signup
+  signup,
 };
+
+function createJWT(user) {
+  return jwt.sign(
+    {user}, // data payload
+    SECRET,
+    {expiresIn: '24h'}
+  );
+}
 
 function signup(req, res) {
   var user = new User(req.body);
   user.save()
     .then(user => {
-      // TODO: Send back a JWT instead of the user
-      res.json(user);
+      res.json({token: createJWT(user)});
     })
-    // User data invalid
+    // User data invalid (probably a duplicate email)
     .catch(err => res.status(400).json(err));
 }
