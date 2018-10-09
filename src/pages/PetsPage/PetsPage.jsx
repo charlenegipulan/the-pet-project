a1e785265b7a83b6804d3b2f4a9ef564c51b481f
Charlene Gipulan <charlenegipulan@gmail.com>
2018-10-09 15:33:36 -0700

    fix nav bar

---
+++ src/pages/PetsPage/PetsPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+class PetsPage extends React.Component {
+
+    render() {
+        return (
+            <div className='PetsPage'>
+
+            </div>
+        )
+    }
+}
+
+export default PetsPage;
