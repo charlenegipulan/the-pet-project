98d5e46aaa3715ea3842cb996e6da8d9a0fedc48
Rachel Barker <rachelbarker001@gmail.com>
2018-10-09 14:37:10 -0700

    starting point for jwt auth

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './LandingPage.css';
 import LandingBanner from '../../components/LandingBanner/LandingBanner';
-import LandingImage from '../../components/LandingCarousel/LandingCarousel';
+import LandingImage from '../../components/LandingImage/LandingImage';
 import NavBar from '../../components/NavBar/NavBar';
 
 class LandingPage extends React.Component {
